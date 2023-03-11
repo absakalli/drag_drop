03428de0b4a977efbf65f8d98e9d32057f2f21d7
absakalli <bugra.sakalli@sersim.com.tr>
2023-03-11 13:54:37 +0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 import { ElementComponent } from './components/element/element.component';
-import { ImportComponent } from './components/import/import.component';
-import { ExportComponent } from './components/export/export.component';
-import { WebApiComponent } from './components/web-api/web-api.component';
-import { PageComponent } from './components/page/page.component';
-import { ToPDFComponent } from './components/to-pdf/to-pdf.component';
 import { ElementFormComponent } from './components/element/element-form/element-form.component';
 import { ElementCardComponent } from './components/element/element-card/element-card.component';
+import { PageComponent } from './components/page/page.component';
 import { PageCardComponent } from './components/page/page-card/page-card.component';
 import { PageFormComponent } from './components/page/page-form/page-form.component';
+import { ExportComponent } from './components/export/export.component';
+import { ImportComponent } from './components/import/import.component';
+import { ToPDFComponent } from './components/to-pdf/to-pdf.component';
+import { WebApiComponent } from './components/web-api/web-api.component';
 
 @NgModule({
   declarations: [
