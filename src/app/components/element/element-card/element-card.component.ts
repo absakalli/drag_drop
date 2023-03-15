8f6a0a1a239ce299237f139ab2cf2f8b3c96d1ef
absakalli <bugra.sakalli@sersim.com.tr>
2023-03-15 11:06:48 +0300

    element card ok

--- src/app/components/element/element-card/element-card.component.ts
+++ src/app/components/element/element-card/element-card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { _Element } from 'src/app/models/element.model';
 import { ElementService } from 'src/app/services/element.service';
 
 @Component({
@@ -9,7 +10,37 @@ import { ElementService } from 'src/app/services/element.service';
 export class ElementCardComponent {
   constructor(public elmServices: ElementService) {}
 
-  isElementHidden() {
-    this.elmServices.isHidden = false;
+  openElmForm() {
+    if (this.elmServices._id != undefined || null || '') {
+      this.elmServices.isHidden = false;
+    } else {
+      alert('Lütfen düzenlemek istedğiniz elementi seçiniz.');
+    }
+  }
+
+  addElement() {
+    //element ekler
+    this.elmServices.element = new _Element(
+      crypto.randomUUID(),
+      150,
+      50,
+      '',
+      '0',
+      '#000000',
+      0,
+      0
+    );
+    this.elmServices.elements.push(this.elmServices.element);
+  }
+
+  removeElement() {
+    //seçili elementi siler
+    if (this.elmServices._index != undefined || null || '') {
+      this.elmServices.elements.splice(this.elmServices._index, 1);
+      this.elmServices._index = null;
+      this.elmServices.clearElementField();
+    } else {
+      alert('Lütfen düzenlemek istedğiniz elementi seçiniz.');
+    }
   }
 }
